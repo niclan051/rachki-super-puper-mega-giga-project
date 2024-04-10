5339682d837bf6d1a92442e24ebfa2a148c18109
niclan051 <89274627917m@gmail.com>
2024-04-10 16:59:21 +0300

    added a way to switch perspective from 3d to 2d + added a utility script to copy position, rotation and scale

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] private Camera camera3D, camera2D;
+
+    private bool _isIn3DMode = true;
+
+    private CinemachineFreeLook _cinemachineFreeLook;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        _cinemachineFreeLook = camera3D.GetComponent<CinemachineFreeLook>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) _isIn3DMode = !_isIn3DMode;
+        camera3D.enabled = _isIn3DMode;
+        camera2D.enabled = !_isIn3DMode;
+
+        _cinemachineFreeLook.enabled = _isIn3DMode;
+    }
+}
